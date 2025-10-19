3bfd2dd9152f39ae422e3c18816b1fca03161188
Venmathi G <venma2004@gmail.com>
2025-10-19 07:30:53 +0530

    cruise-ship

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import AllRoute from "./Routes/AllRoute";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function App() {
+  return (
+    <>
+      <AllRoute />
+      <ToastContainer />
+    </>
+  );
+}
+
+export default App;
